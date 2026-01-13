ae0170a328a963a4bd9e9b3bdbdde75db9d1b8dd
am-ramona <ramona@karpatkey.com>
2026-01-13 22:21:39 +0200

    Change typewrited sentence effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,6 +132,22 @@
       }
     }
 
+      /**
+   * Init typed.js
+   */
+  const selectTyped = document.querySelector('.typed');
+  if (selectTyped) {
+    let typed_strings = selectTyped.getAttribute('data-typed-items');
+    typed_strings = typed_strings.split(',');
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 70,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
+
     // Get the button element
     let backToTopIcon = document.querySelector('#back-to-top');
 
